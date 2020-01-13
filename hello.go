59a70b091ac90404bafedd3f8e72106e36f778a6
Johnny Packard <johnny@johnnypackard.com>
2020-01-13 15:08:56 -0600

    adding tests

--- hello.go
+++ hello.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/johnnypackard/golang/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
